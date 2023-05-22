c31ab951bb6f06af27491cb48142017b174de74a
atippaz <nasakun13201@gmail.com>
2023-05-22 11:56:01 +0700

    reverse

--- plugins/api/endpoint.js
+++ plugins/api/endpoint.js
@@ -1,2 +1,2 @@
-// export const endpoint = 'https://localhost:5000/'
-export const endpoint = 'https://6137-119-110-237-112.ngrok-free.app/'
+export const endpoint = 'https://localhost:5000/'
+// export const endpoint = 'https://6137-119-110-237-112.ngrok-free.app/'
